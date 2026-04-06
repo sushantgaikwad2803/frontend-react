0377268ce678c5763387eed9f397e11add3909c8
Sushant Gaikwad <sushantgaikwad2803@gmail.com>
2026-04-06 19:52:44 +0530

    Update InvestorsSection.js

--- src/components/InvestorsSection.js
+++ src/components/InvestorsSection.js
@@ -1,19 +1,7 @@
 import React from "react";
 import "./InvestorsSection.css";
-// import M1 from '../media/blackroack.jpg';
-// import M2 from '../media/fidelity.jpg';
-// import M3 from '../media/j.p.png';
-// import M4 from '../media/Vanguard.png';
-// import M5 from '../media/Allianz.png';
 
 export default function InvestorsSection() {
-  // const logos = [
-  //   M1 ,
-  //   M2 ,
-  //   M3 ,
-  //   M4 ,
-  //   M5 
-  // ]; 
 
   return (
     <div className="investor-wrapper">
@@ -21,12 +9,6 @@ export default function InvestorsSection() {
       <p className="investor-subtitle">
         Investors from around the world turn to AnnualReports.com for timely insights into global business activity.
       </p>
-
-      {/* <div className="investor-logos">
-        {logos.map((logo, index) => (
-          <img key={index} src={logo} alt="Investor Logo" className="investor-logo" />
-        ))}
-      </div> */}
     </div>
   );
 }
