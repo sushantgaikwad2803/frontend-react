3f1948f23b08e8dd819bd44fbce702654cc01f91
Sushant Gaikwad <sushantgaikwad2803@gmail.com>
2026-04-06 20:11:57 +0530

    Update InvestorsSection.js

--- src/components/InvestorsSection.js
+++ src/components/InvestorsSection.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./InvestorsSection.css";
 
 export default function InvestorsSection() {
+
   return (
-    <div>
-      <h2>Investors Section</h2>
+    <div className="investor-wrapper">
+      <h2 className="investor-title">Used by Top Institutional Investors Around the Global</h2>
+      <p className="investor-subtitle">
+        Investors from around the world turn to AnnualReports.com for timely insights into global business activity.
+      </p>
     </div>
   );
 }
